a69f35c85da86472466afb6ef850e851562b3beb
Amoryne <amoryne.jolimont@gmail.com>
2022-07-05 12:47:43 +0200

    test

---
+++ src/Components/Item.js
@@ -0,0 +1,13 @@
+
+export default function Item(props) {
+  return (
+
+
+    <li className="border justify-content-between align-items-center d-flex p-2 m-2">
+        <div className="p-3">{props.txt}</div>
+        <button onClick={()=> props.delFunc(props.id)} className="btn btn-danger p-2 h-50">supprimer</button>
+    </li>
+
+
+  )
+}
